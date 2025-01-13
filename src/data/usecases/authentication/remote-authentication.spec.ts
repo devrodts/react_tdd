813af40e5c205beecdbaa596aaadf15a51c40edb
Rodolfo <dev_rodts@outlook.com>
2025-01-13 19:22:13 -0300

    test: ensure RemoteAuth throws UnexpectedError on 400

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,6 +4,7 @@ import { faker } from "@faker-js/faker"
 import { mockAuthentication } from "@/domain/test/mock-authentication"
 import { HttpStatusCode } from "@/data/protocols/http/http-response"
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error"
+import { UnexpectedError } from "@/domain/errors/unexpected-error"
 
 
 
@@ -38,7 +39,7 @@ describe('RemoteAuthentication', () => {
     })
 
     test('Should throw UnauthorizedError if HttpPostClient returns 401', async () => {
-        
+
         const { sut, httpPostClientSpy } = makeSut()
         httpPostClientSpy.response = {
             statusCode: HttpStatusCode.unauthorized
@@ -46,5 +47,16 @@ describe('RemoteAuthentication', () => {
 
         const promise = sut.auth(mockAuthentication())
         await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+    })
+
+    test('Should throw UnexpectedError if HttpPostClient returns 400', async () => {
+        
+        const { sut, httpPostClientSpy } = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.badRequest
+        }
+
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new UnexpectedError())
     })
 })
